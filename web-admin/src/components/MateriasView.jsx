9def564770b4f496d10f1b8bbb8ce24272b984b1
Alexis <carapedoxd@gmail.com>
2026-02-11 23:32:16 -0500

    omitir error

--- web-admin/src/components/MateriasView.jsx
+++ web-admin/src/components/MateriasView.jsx
@@ -44,6 +44,7 @@ export default function MateriasView() {
     }, []); 
 
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         cargarDatos();
     }, [cargarDatos]);
 
